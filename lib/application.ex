71502012f837dc35224d94bfa2d907800e691249
nklhtv <nklhtv@gmail.com>
2025-09-07 15:11:08 +0300

    chore: code reformat

--- lib/application.ex
+++ lib/application.ex
@@ -2,11 +2,12 @@ defmodule Redis.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      RedisSupervisor
-    ]
-
-    opts = [strategy: :one_for_one, name: __MODULE__]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(
+      [
+        RedisSupervisor
+      ],
+      strategy: :one_for_one,
+      name: __MODULE__
+    )
   end
 end
--- lib/redis_supervisor.ex
+++ lib/redis_supervisor.ex
@@ -1,16 +1,15 @@
 defmodule RedisSupervisor do
   use Supervisor
 
-  def start_link(init_arg) do
-    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
-  end
+  def start_link(init_arg), do: Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
 
   @impl true
   def init(_init_arg) do
-    children = [
-      Redis
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.init(
+      [
+        Redis
+      ],
+      strategy: :one_for_one
+    )
   end
 end
