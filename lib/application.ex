cd6608b8ecb5acdbce4edffba2d9c7ce931cb6ca
nklhtv <nklhtv@gmail.com>
2025-09-07 15:09:07 +0300

    add application & supervisor

---
+++ lib/application.ex
@@ -0,0 +1,12 @@
+defmodule Redis.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      RedisSupervisor
+    ]
+
+    opts = [strategy: :one_for_one, name: __MODULE__]
+    Supervisor.start_link(children, opts)
+  end
+end
